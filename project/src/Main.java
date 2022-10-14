2a71f3a22a4e82f81aac443eeaa182102ab3d014
Juan Puertas <juanpuertas486@gmail.com>
2022-10-13 20:43:40 -0400

    EjercicioTema3Completo

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+
+        System.out.println(suma(10,10,10));
+
+        coche micoche=new coche();
+        micoche.AñadirPuertas();
+        micoche.AñadirPuertas();
+        micoche.AñadirPuertas();
+
+        System.out.println("Numero de puertas del coche:" + micoche.puerta);
     }
+    public static int suma(int a , int b, int c){
+        return a+b+c;
+
+    }
+}
+
+class coche
+{
+    public int puerta=0;
+
+    public void AñadirPuertas(){
+        this.puerta++;
+    }
+
+
 }
